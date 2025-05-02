e94fae5f98379ad2eae3cbd9c59224cda76b1742
RArora <130247456+RituArora-DevOps@users.noreply.github.com>
2025-05-02 14:02:33 -0400

    MVC vs 3-tier architecture

--- src/main/java/com/oss/ossv1/data/repository/ProductRepository.java
+++ src/main/java/com/oss/ossv1/data/repository/ProductRepository.java
@@ -4,5 +4,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.oss.ossv1.data.entity.Product;
 
+/**
+ * This interface gives you methods like:
+ * save(product)
+ * findById(id)
+ * findAll()
+ * deleteById(id)
+ *
+ * You can use them directly in your ProductService or ProductController.
+ */
 public class ProductRepository extends JpaRepository<Product, Long> {
+    // You don't need to add anything here
 }
