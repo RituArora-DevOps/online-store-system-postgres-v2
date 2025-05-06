1a343b469caecfb66d08da2ce8a9f1822038884f
RArora <130247456+RituArora-DevOps@users.noreply.github.com>
2025-05-06 09:33:04 -0400

    created UI folder

---
+++ src/main/java/com/oss/ossv1/ui/CartView.java
@@ -0,0 +1,24 @@
+package com.oss.ossv1.ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class CartView extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Hello, JavaFX 24!");
+        Scene scene = new Scene(label, 400, 200);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("JavaFX 24 Demo");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
