9d95b1039ab313997dcf70cab4aba5d88731a8e0
Shao Hang Li <shao.hang.li@outlook.com>
2025-05-26 12:08:32 -0400

    Removed

--- src/main/java/com/oss/ossv1/behavioral/ObserverPattern/ProductReview.java
+++
@@ -1,4 +0,0 @@
-package com.oss.ossv1.behavioral.ObserverPattern;
-
-public class ProductReview {
-}
--- src/main/java/com/oss/ossv1/behavioral/ObserverPattern/ReviewObserver.java
+++
@@ -1,9 +0,0 @@
-package com.oss.ossv1.behavioral.ObserverPattern;
-
-/**
- * Observer interface for the Observer pattern
- * Defines the update method that gets called when a new review is added
- */
-public interface ReviewObserver {
-    void update(ProductReview review);
-}
