5a69e7a2710c716490ddf850c1fdd08f466cc9f2
Shao Hang Li <shao.hang.li@outlook.com>
2025-05-28 09:13:00 -0400

    added Review and behavioral system with the admin panel

--- src/main/java/com/oss/ossv1/data/repository/UserRepository.java
+++ src/main/java/com/oss/ossv1/data/repository/UserRepository.java
@@ -1,12 +1,20 @@
 package com.oss.ossv1.data.repository;
 
-import com.oss.ossv1.data.entity.User;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.Optional;
+
+import com.oss.ossv1.data.entity.User;
 
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUsername(String username);
+    
+    /**
+     * Find all users who are administrators
+     */
+    List<User> findByIsAdminTrue();
 }
